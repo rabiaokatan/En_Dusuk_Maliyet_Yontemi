3bdfffb61fd1cf3407e0bc983d4e9547573adcf9
Rabia Okatan <rabiaro26@gmail.com>
2021-11-08 18:13:05 +0300

    arz-talep labelları eklendi

--- endusukmaliyet/Form1.cs
+++ endusukmaliyet/Form1.cs
@@ -29,12 +29,15 @@ namespace endusukmaliyet
 
             panel1.Controls.Clear();
 
-            for (int i = 0; i < satir+1; i++)
+            for (int i = 0; i < satir + 1; i++)
             {
-                X_coordinate = loc.X;
+                X_coordinate = loc.X + 30;
                 Y_coordinate = Y_coordinate + 30;
 
-                for (int j = 0; j < sutun+1; j++)
+                Label l = new Label();
+                Label ll = new Label();
+
+                for (int j = 0; j < sutun + 1; j++)
                 {
                     TextBox a = new TextBox();
                     a.Width = 30;
@@ -42,9 +45,26 @@ namespace endusukmaliyet
 
                     X_coordinate += 35;
 
-                    a.Text = (i + 1).ToString();
+                    a.Text = ("[" + (i + 1) + "," + (j + 1) + "]").ToString();
                     panel1.Controls.Add(a);
                     panel1.Show();
+
+
+                    if (j == sutun)
+                    {
+                        l.Text = "ARZ";
+                        panel1.Controls.Add(l);
+                        panel1.Show();
+                        l.Location = new Point( X_coordinate , loc.Y + 30);
+                    }
+                    else if (i == satir)
+                    {
+                        ll.Text = "TALEP";
+                        panel1.Controls.Add(ll);
+                        panel1.Show();
+                        ll.Location = new Point((loc.X), Y_coordinate+35);
+                    }
+
 
                     a.Location = new Point(X_coordinate, Y_coordinate + 30);
 
